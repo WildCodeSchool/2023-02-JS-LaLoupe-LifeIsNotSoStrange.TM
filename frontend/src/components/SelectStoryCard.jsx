390ae3d6f7a1bd722f776d70469a0ff1cb0103e6
Luc <lefebureluc.42@gmail.com>
2023-04-14 10:34:59 +0200

    responsive img

--- frontend/src/components/SelectStoryCard.jsx
+++ frontend/src/components/SelectStoryCard.jsx
@@ -4,23 +4,23 @@ const selectStoryList = [
   {
     tittle: "Toronto by night",
     summary: "A dark adventure on the streets of Toronto city.",
-    image: "https://i.postimg.cc/Mp9GCj0t/detective-city.jpg",
+    image: "./src/assets/detective_city.jpg",
   },
   {
     tittle: "Back to the Maya's life !",
     summary: "This time you took your dictionary.",
-    image: "https://i.postimg.cc/2S1p5bR0/maya-story.jpg",
+    image: "./src/assets/maya-story.jpg",
   },
   {
     tittle: "Where is Aqua ?",
     summary: "Aqua is a very special color...",
-    image: "https://i.postimg.cc/02WRxG36/Who-s-is-Aqua-colors.jpg",
+    image: ".src/assets/Who-s-is-Aqua-colors.jpg",
   },
   {
     tittle: "Xavier and the little canibal unicorns ponies.",
     summary:
       "the question that we can ask ourselves and why xavier said: 'oh no not yet!'",
-    image: "https://i.postimg.cc/VLLQ3Z7Q/little-ponie.jpg",
+    image: "./src/assets/little-ponie.jpg",
   },
 ];
 
@@ -30,7 +30,7 @@ const SelectStoryCard = () => {
     <div className="bg-amber-400 p-8 flex flex-col items-center rounded-lg justify-center max-md:m-16">
       <h4 className="pt-4 text-2xl">{selectStory.tittle}</h4>
       <img
-        className="px-8 py-8 max-lg: rounded-md"
+        className=" object-contain px-8 py-8 max-lg: rounded-md"
         src={selectStory.image}
         alt=""
       />
