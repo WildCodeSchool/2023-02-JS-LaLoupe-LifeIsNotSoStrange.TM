24e5b8ef6ed09a0e79432f39d5cf7ca6b8e4d83f
Luc <lefebureluc.42@gmail.com>
2023-04-19 12:30:51 +0200

    Merge branch 'dev' into components-select-story

--- frontend/src/components/SelectStoryCard.jsx
+++ frontend/src/components/SelectStoryCard.jsx
@@ -1,48 +1,30 @@
 import React from "react";
+import { PropTypes } from "prop-types";
 
-const selectStoryList = [
-  {
-    tittle: "Toronto by night",
-    summary: "A dark adventure on the streets of Toronto city.",
-    image: "./src/assets/detective_city.jpg",
-  },
-  {
-    tittle: "Back to the Maya's life !",
-    summary: "This time you took your dictionary.",
-    image: "./src/assets/maya-story.jpg",
-  },
-  {
-    tittle: "Where is Aqua ?",
-    summary: "Aqua is a very special color...",
-    image: ".src/assets/panel-colors.png",
-  },
-  {
-    tittle: "Xavier and the little canibal unicorns ponies.",
-    summary:
-      "the question that we can ask ourselves and why xavier said: 'oh no not yet!'",
-    image: "./src/assets/little-ponie.jpg",
-  },
-];
-
-const SelectStoryCard = () => {
-  const selectStory = selectStoryList[2];
+export default function SelectStoryCard({ title, image, summary }) {
   return (
-    <div className="bg-amber-400 p-8 flex flex-col items-center rounded-lg max-w-xs justify-center max-md:m-16">
-      <h4 className=" font-mono font-extrabold text-center text-xs">
-        {selectStory.tittle}
+    <div className="bg-gray-600 p-8 flex flex-col items-center rounded-lg max-w-xs justify-center m-8">
+      <h4 className=" border-2 rounded-md font-mono font-extrabold text-center text-xs text-white">
+        {title}
       </h4>
-      <img className=" rounded-md my-4" src={selectStory.image} alt="" />
-      <p className=" text-center font-mono font-extrabold text-xs">
-        {selectStory.summary}
+      <img className="object-contain rounded-md my-4" src={image} alt="" />
+      <p className="text-center text-white font-mono font-extrabold text-xs">
+        {summary}
       </p>
       <button
-        className="bg-teal-300 font-mono p-1 mt-3 rounded-md px-3"
+        className="bg-pink-200 font-mono p-1 mt-3 rounded-md px-3"
         type="button"
       >
         Play
       </button>
     </div>
   );
-};
+}
 
-export default SelectStoryCard;
+SelectStoryCard.propTypes = {
+  SelectStoryCard: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    image: PropTypes.string,
+    summary: PropTypes.string,
+  }).isRequired,
+};
