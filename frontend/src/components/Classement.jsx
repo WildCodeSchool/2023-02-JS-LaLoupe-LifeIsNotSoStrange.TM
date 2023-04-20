ebd210c40cb32e74559752b420a7abd5de086b80
Cassandra <kassy.loulou61@gmail.com>
2023-04-20 09:45:14 +0200

    add page classement and classement.css, responsive and add route for page classement

---
+++ frontend/src/components/Classement.jsx
@@ -0,0 +1,52 @@
+import "./Classement.css";
+
+function Classement() {
+  return (
+    <div className="tableau">
+      <table>
+        <thead>
+          <tr>
+            <th>Pseudo</th>
+            <th>Avatar</th>
+            <th>Classement</th>
+            <th>Timer</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Xavier</td>
+            <td>???</td>
+            <td>1er</td>
+            <td>00:00</td>
+          </tr>
+          <tr>
+            <td>Xavier</td>
+            <td>???</td>
+            <td>1er</td>
+            <td>00:00</td>
+          </tr>
+          <tr>
+            <td>Xavier</td>
+            <td>???</td>
+            <td>1er</td>
+            <td>00:00</td>
+          </tr>
+          <tr>
+            <td>Xavier</td>
+            <td>???</td>
+            <td>1er</td>
+            <td>00:00</td>
+          </tr>
+          <tr>
+            <td>Xavier</td>
+            <td>???</td>
+            <td>1er</td>
+            <td>00:00</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Classement;
