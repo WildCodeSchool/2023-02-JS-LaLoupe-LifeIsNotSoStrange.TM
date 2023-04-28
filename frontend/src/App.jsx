32dec6679d6b6699c0ccbec02fced93dd7deaf5f
Florian ESNAULT <florian61340@hotmail.fr>
2023-04-28 10:34:32 +0200

    Added the possibility to create an avatar for the player and a state to display it during the game

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
@@ -6,16 +7,49 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import PartyGame from "./components/PartyGame";
 import Leaderboard from "./components/Leaderboard";
+import SelectStoryList from "./components/SelectStoryList";
 
 function App() {
+  const [avatarParams, setAvatarParams] = useState({
+    avatarStyle: "Transparent",
+    accessoriesType: "Round",
+    topType: "LongHairBob",
+    clotheColor: "Blue02",
+    clotheType: "ShirtCrewNeck",
+    eyeType: "Surprised",
+    eyebrowType: "SadConcernedNatural",
+    facialHairColor: "Black",
+    facialHairType: "BeardLight",
+    hairColor: "Brown",
+    mouthType: "Twinkle",
+    skinColor: "Pale",
+    pseudo: "",
+  });
   return (
     <div className="min-h-screen relative pb-[60px]">
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          element={
+            <Home
+              avatarParams={avatarParams}
+              setAvatarParams={setAvatarParams}
+            />
+          }
+        />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/game" element={<PartyGame />} />
+        <Route
+          path="/game"
+          element={
+            <PartyGame
+              avatarParams={avatarParams}
+              pseudo={avatarParams.pseudo}
+            />
+          }
+        />
         <Route path="/leaderboard" element={<Leaderboard />} />
+        <Route path="/selectstory" element={<SelectStoryList />} />
       </Routes>
       <Footer />
     </div>
--- frontend/src/components/PartyGame.jsx
+++ frontend/src/components/PartyGame.jsx
@@ -1,13 +1,35 @@
+import PropTypes from "prop-types";
 import Timer from "./Timer";
 import Ecran from "./Ecran";
 import Story from "./Story";
+import AvatarDisplay from "./AvatarDisplay";
 
-export default function PartyGame() {
+export default function PartyGame({ avatarParams }) {
   return (
     <div>
+      <AvatarDisplay avatarParams={avatarParams} />
+      <p>{avatarParams.pseudo}</p>
       <Timer />
       <Ecran />
       <Story />
     </div>
   );
 }
+
+PartyGame.propTypes = {
+  avatarParams: PropTypes.shape({
+    avatarStyle: PropTypes.string.isRequired,
+    accessoriesType: PropTypes.string.isRequired,
+    topType: PropTypes.string.isRequired,
+    clotheColor: PropTypes.string.isRequired,
+    clotheType: PropTypes.string.isRequired,
+    eyeType: PropTypes.string.isRequired,
+    eyebrowType: PropTypes.string.isRequired,
+    facialHairColor: PropTypes.string.isRequired,
+    facialHairType: PropTypes.string.isRequired,
+    hairColor: PropTypes.string.isRequired,
+    mouthType: PropTypes.string.isRequired,
+    skinColor: PropTypes.string.isRequired,
+    pseudo: PropTypes.string.isRequired,
+  }).isRequired,
+};
