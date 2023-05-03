6641b38bc5b6b19f03060b24c9625a8166657f3a
Luc <lefebureluc.42@gmail.com>
2023-05-03 12:33:40 +0200

    fix css for the select party-games

--- frontend/src/components/SelectStoryCard.jsx
+++ frontend/src/components/SelectStoryCard.jsx
@@ -1,31 +1,30 @@
 import React from "react";
+import "./SelectStoryCard.css";
 import { PropTypes } from "prop-types";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 export default function SelectStoryCard({ title, image, summary }) {
   return (
-    <div className="bg-gray-600 grid p-8 items-center rounded-lg max-w-xs justify-center m-8">
-      <h4 className=" border-2 rounded-md font-mono font-extrabold text-center text-xs text-white">
-        {title}
-      </h4>
-      <img
-        className="object-contain rounded-md my-4 shadow-xl"
-        src={image}
-        alt=""
-      />
-      <p className="text-center text-white font-mono font-extrabold text-xs">
-        {summary}
-      </p>
-
-      <Link to="/game">
-        <button
-          className="bg-white font-mono p-1 mt-3 rounded-md px-3 w-[150px] text-black m-auto flex justify-center"
-          type="button"
-        >
-          Play
-        </button>
-      </Link>
-    </div>
+    <section className="">
+      <div className="card-container">
+        <div className="card-content">
+          <div className="card-title">
+            <span className="title">{title}</span>
+          </div>
+          <div className="card-body">
+            <img src={image} alt="" />
+          </div>
+          <div className="card-footer">
+            <span className="title">{summary}</span>
+          </div>
+          {/* <Link to="/game">
+            <button className="title" type="button">
+              Play
+            </button>
+          </Link> */}
+        </div>
+      </div>
+    </section>
   );
 }
 
